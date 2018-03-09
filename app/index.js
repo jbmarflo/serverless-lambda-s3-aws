6a505aba9f6e3d71bd0174a92316f2b33fff37d9
Joseph Marcilla <joseph@yarkan.pe>
2018-03-09 18:40:50 -0500

    feat(request): instalado request para peticion

--- app/index.js
+++ app/index.js
@@ -1,5 +1,16 @@
-
+var AWS = require("aws-sdk");
+var SQS = AWS.SQS();
+var request = require('request');
+var url ='https://requestb.in/1fx3ofb1';
 
 exports.handler = function(event, context, callback) {
 
+
+    request.post({
+        headers: {'content-type' : 'application/json'},
+        url:     url,
+        body:    "mes=heydude"
+    }, function(error, response, body){
+        console.log(body);
+    });
 };
