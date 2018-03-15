62055d8c344faa3a06704b12b5491d8a488c7610
Joseph Marcilla <joseph@yarkan.pe>
2018-03-15 18:10:00 -0500

    serverless

--- app/Process/transformData.js
+++ app/Process/transformData.js
@@ -1,5 +1,5 @@
 var Promise = require('promise');
-
+var https = require("https");
 /**
  *
  * @param parameters
@@ -21,9 +21,9 @@ Transform.prototype.generate = function Transform_generate() {
 Transform.prototype.buildMessages = function Transform_buildMessages() {
     return new Promise(function(resolve, reject){
         if (this.threads instanceof Array) {
-            console.log(this.threads);
             this.threads.map(function(thread, index){
                 var msg = this.parameters;
+                var searchId = msg.searchId;
                 msg.filter.threadId = +index;
                 msg.supplier = thread.supplier;
 
@@ -31,9 +31,44 @@ Transform.prototype.buildMessages = function Transform_buildMessages() {
                     msg.filter = Object.assign(msg.filter, thread.filters);
                 }
 
-                this.messages.push(msg);
-            }.bind(this));
+                var timeout_function = function( req ) {
+                    return function( ) {
+                        req.abort( );
+                    };
+                };
+                var timeout = msg.timeout;
+                var data = JSON.stringify(msg);
+                var options = {
+                    host: "hookb.in",
+                    port: 443,
+                    path: "/KlwwGdjD",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Content-Length": Buffer.byteLength(data)
+                    }
+                };
 
+                var req = https.request(options, function (res) {
+                    var response = "";
+
+                    res.setEncoding("utf8");
+
+                    res.on("data", function (chunk) {
+                        response += chunk;
+                    });
+
+                    res.on("end", function () {
+                        console.log(response);
+                    });
+                });
+                req.write(data);
+                var fn = timeout_function(req);
+                var execTimeout = setTimeout( fn, timeout * 1000 );
+
+                //Put finish service searchId
+                req.end();
+            }.bind(this));
         }
         resolve(this.parameters);
     }.bind(this));
--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var AWS = require("aws-sdk");
 var S3 = require("./persistence/s3");
 var Transform = require("./Process/transformData");
 // var SQS = AWS.SQS();
-var https = require("https");
+
 // var url ='https://hookb.in/ZjLLxW1D';
 
 exports.handler =  function(event, context, callback) {
@@ -25,38 +25,38 @@ exports.handler =  function(event, context, callback) {
         .then(function(data) {
             var parsedData = JSON.parse(data.Body.toString('utf-8'));
             var objTransform = new Transform(parsedData);
-            objTransform.generate()
-                .then(function(obj) {
-                    obj.map(function (message) {
-                        var timeout = message.timeout;
-                        var data = JSON.stringify(message);
-                        var options = {
-                            host: "hookb.in",
-                            port: 443,
-                            path: "/ZjLLxW1D",
-                            method: "POST",
-                            headers: {
-                                "Content-Type": "application/json",
-                                "Content-Length": Buffer.byteLength(data)
-                            }
-                        };
-                        var req = https.request(options, function (res) {
-                            var response = "";
-
-                            res.setEncoding("utf8");
-
-                            res.on("data", function (chunk) {
-                                response += chunk;
-                            });
-
-                            res.on("end", function () {
-                                // console.log(response);
-                            });
-                        });
-                        req.write(data);
-                        req.end();
-                    });
-                });
+            objTransform.generate();
+                // .then(function(obj) {
+                //     obj.map(function (message) {
+                //         var timeout = message.timeout;
+                //         var data = JSON.stringify(message);
+                //         var options = {
+                //             host: "hookb.in",
+                //             port: 443,
+                //             path: "/ZjLLxW1D",
+                //             method: "POST",
+                //             headers: {
+                //                 "Content-Type": "application/json",
+                //                 "Content-Length": Buffer.byteLength(data)
+                //             }
+                //         };
+                //         var req = https.request(options, function (res) {
+                //             var response = "";
+                //
+                //             res.setEncoding("utf8");
+                //
+                //             res.on("data", function (chunk) {
+                //                 response += chunk;
+                //             });
+                //
+                //             res.on("end", function () {
+                //                 // console.log(response);
+                //             });
+                //         });
+                //         req.write(data);
+                //         req.end();
+                //     });
+                // });
         });
 
     callback(null, 'Message processed');
